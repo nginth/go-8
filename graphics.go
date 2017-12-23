da99d5df9ad60d5afe0b94f4b13b58ae6d9e87cc
Nick Ginther <nginth@gmail.com>
2017-12-23 17:34:57 -0600

    draw window to screen with GLFW

--- graphics.go
+++ graphics.go
@@ -1,5 +1,38 @@
 package main
 
-func setupGraphics() {
+import "github.com/go-gl/glfw/v3.2/glfw"
+
+const (
+	width  = 640
+	height = 320
+)
+
+func setupGraphics() *glfw.Window {
+	err := glfw.Init()
+	check(err)
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "GO-8", nil, nil)
+	check(err)
+
+	window.MakeContextCurrent()
+	return window
+}
+
+func terminateGraphics() {
+	glfw.Terminate()
+}
+
+func updateWindow(window *glfw.Window) {
+	window.SwapBuffers()
+	glfw.PollEvents()
+}
+
+func render(window *glfw.Window, gfx []uint8) {
 
 }
--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
+import "runtime"
+
+func init() {
+	runtime.LockOSThread()
+}
+
 func main() {
-	setupGraphics()
+	window := setupGraphics()
+	defer terminateGraphics()
 	// setupInput
 	go8 := Go8{}
 	// initialize
-	go8.V[0] = 12
 	go8.initialize()
 	// TODO: load ROM
 	// go8.loadROM("rom")
 
 	// emulation loop
-	for {
+	for !window.ShouldClose() {
 		go8.emulateCycle()
+		updateWindow(window)
 		if go8.drawFlag != 0 {
 			// TODO: draw graphics
 		}
